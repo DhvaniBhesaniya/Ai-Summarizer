5808508a6f4be9257754f57eeb098b64ede716cd
DhvaniBhesaniya <narutogokuluffy009@gmail.com>
2024-07-31 15:19:25 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ai-summarizer/',
+  base: '/Ai-Summarizer/', // Ensure this matches your repository name exactly
   plugins: [react()],
-});
+})
